94bc93602a7137a1af79f544026b0e14d6012cdb
João Zamarian <160238830+xxfellas@users.noreply.github.com>
2024-11-07 23:53:05 -0400

    Add files via upload

---
+++ Des-Web/Des-Web/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'laboratorio/des-web',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5914db6bb772bdb7e5974efc310e8f71455dd31d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'laboratorio/des-web' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5914db6bb772bdb7e5974efc310e8f71455dd31d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
